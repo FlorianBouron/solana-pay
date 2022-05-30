b9f26cbaa9db89acc344d63eb31217b5e2069283
Florian Bouron <bouron.florian@yahoo.fr>
2022-05-30 21:18:47 +0200

    Add Solana Pay sandbox

---
+++ README.md
@@ -0,0 +1,13 @@
+# Store on Solana Pay
+
+This repository is a POC for testing Solana Pay as well as the developer experience.
+Nothing fancy, the code is not formatted, and just here to work quickly in a few hours of work.
+Please do not throw up by having a look at that code :)
+
+### Command lines
+
+To get started with this project, clone this repo and follow these commands:
+
+1. Run `yarn install` at the root of your directory
+2. Run `yarn dev` to start the project
+3. Start coding!
---
+++ pages/index.js
@@ -0,0 +1,76 @@
+import { useState, useEffect } from "react";
+import CreateProduct from "../components/CreateProduct";
+import Product from "../components/Product";
+import HeadComponent from "../components/Head";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+
+const renderNotConnectedContainer = () => (
+  <div>
+    <img
+      src="https://media0.giphy.com/media/LveMkKaN3KWcg/giphy.gif"
+      alt="cool kids"
+    />
+    <div className="button-container">
+      <WalletMultiButton className="cta-button connect-wallet-button" />
+    </div>
+  </div>
+);
+
+const App = () => {
+  const { publicKey } = useWallet();
+  const isOwner = publicKey
+    ? publicKey.toString() === process.env.NEXT_PUBLIC_OWNER_PUBLIC_KEY
+    : false;
+  const [creating, setCreating] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    if (publicKey) {
+      fetch(`/api/fetchProducts`)
+        .then((response) => response.json())
+        .then((data) => {
+          setProducts(data);
+          console.log("Products", data);
+        });
+    }
+  }, [publicKey]);
+
+  const renderItemBuyContainer = () => (
+    <div className="products-container">
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  );
+
+  return (
+    <div className="App">
+      <HeadComponent />
+      <div className="container">
+        <header className="header-container">
+          <p className="header">👟 Sneakers Store 👟</p>
+          <p className="sub-text">
+            The only sneakers store that accepts cool kids token
+          </p>
+
+          {isOwner && (
+            <button
+              className="create-product-button"
+              onClick={() => setCreating(!creating)}
+            >
+              {creating ? "Close" : "Create Product"}
+            </button>
+          )}
+        </header>
+
+        <main>
+          {creating && <CreateProduct />}
+          {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default App;
